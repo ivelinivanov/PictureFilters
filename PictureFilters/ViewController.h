69d101588ad5ac2cddc4ddfdbd9891430e2a1de3
Ivelin Ivanov <ivelin.ivanov@Student01s-Mac-mini.local>
2013-09-02 15:31:50 +0300

    Initial Commit

---
+++ PictureFilters/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PictureFilters
+//
+//  Created by Ivelin Ivanov on 9/2/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
